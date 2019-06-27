063647aa8b35dc00e9d09158b56509bbf76b9ca9
Viet-Trung Luu <vtl@tricot.io>
2019-06-26 17:52:39 -0700

    [build][circleci] Use a script to build everything.

---
+++ .circleci/build_all.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright 2019 Tricot Inc.
+# Use of this source code is governed by the license in the LICENSE file.
+
+# Note: This script assumes that the environment has been set up.
+
+TARGET_DIRS=(
+    examples/ws_client
+    examples/ws_client_test
+)
+
+set -e
+
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE}")/.."; pwd)"
+
+echo "*** Root dir: ${ROOT_DIR}"
+
+for TARGET_DIR in "${TARGET_DIRS[@]}"; do
+    echo
+    echo "*** Building: ${TARGET_DIR}"
+    echo
+
+    cd "${ROOT_DIR}"
+    cd "${TARGET_DIR}"
+
+    idf.py build
+
+    echo
+    echo "*** Finished building: ${TARGET_DIR}"
+done
