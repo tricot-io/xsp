d713f9b10870a9e1354e87125f1a931ce1444143
Viet-Trung Luu <vtl@tricot.io>
2019-07-02 16:59:31 -0700

    [loop] It's a start: at least now it (theoretically) loops.

--- components/xsp_loop/include/xsp_loop.h
+++ components/xsp_loop/include/xsp_loop.h
@@ -4,147 +4,48 @@
 #ifndef XSP_LOOP_H_
 #define XSP_LOOP_H_
 
-//FIXME
-#if 0
-#include <stdbool.h>
-
 #include "esp_err.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// NOTES
-//
-// *   The loop receives *frames* and provides them to the user (to be defragmented into messages if
-//     desired at another layer).
-//     *   Users may not need to defragment, or may not be able to defragment.
-//     *   Users may want to set their own policy for message data allocation and storage.
-// *   It sends *messages* provided by the user (fragmenting them as required).
-//     *   This makes it easy to use.
-//     *   Note however that this precludes sending "streamed" messages (i.e., a message whose data
-//         is provided dynamically).
-// *   It receives and sends frames *synchronously*.
-//     *   This is because `xsp_ws_client` is synchronous.
-// *   It sends messages asynchronously.
-//     *   This allows larger messages to be sent without blocking, and frames to be received
-//         while doing so (for multi-frame messages).
-
-typedef struct xsp_ws_client_loop_config {
-    int max_frame_read_size;        // Must be at least 125.
-    int max_data_frame_write_size;  // Must be at least 1.
-
+typedef struct xsp_loop_config {
     int poll_timeout_ms;
-    int read_timeout_ms;
-    int write_timeout_ms;
-} xsp_ws_client_loop_config_t;
+} xsp_loop_config_t;
 
-typedef struct xsp_ws_client_loop* xsp_ws_client_loop_handle_t;
+typedef struct xsp_loop* xsp_loop_handle_t;
 
-// WebSocket client loop events.
-typedef enum xsp_ws_client_loop_event_type {
-    // First event generated each time the event loop is run.
-    XSP_WS_CLIENT_LOOP_EVENT_STARTED = 0,
+typedef void (*on_loop_start_func_t)(xsp_loop_handle_t loop, void* ctx);
+typedef void (*on_loop_stop_func_t)(xsp_loop_handle_t loop, void* ctx);
+typedef void (*on_loop_idle_func_t)(xsp_loop_handle_t loop, void* ctx);
 
-    // Last event generated each time the event loop is run.
-    XSP_WS_CLIENT_LOOP_EVENT_STOPPED,
-
-    // Event generated when no work was done.
-    // NOTE: To prevent spinning, you may want to briefly sleep on this event.
-    XSP_WS_CLIENT_LOOP_EVENT_IDLE,
-
-    // Event generated when the connection is closed (or failed). The loop will terminate
-    // immediately afterwards.
-    XSP_WS_CLIENT_LOOP_EVENT_CLOSED,
-
-    // Event generated when a data (non-control) frame is received.
-    XSP_WS_CLIENT_LOOP_EVENT_DATA_FRAME_RECEIVED,
-
-    // Event generated when a ping frame is received.
-    XSP_WS_CLIENT_LOOP_EVENT_PING_RECEIVED,
-
-    // Event generated when a pong frame is received.
-    XSP_WS_CLIENT_LOOP_EVENT_PONG_RECEIVED,
-
-    // Event generated when a message scheduled using `xsp_ws_client_loop_send_message()` has
-    // completed transmission (or the transmission has failed).
-    XSP_WS_CLIENT_LOOP_EVENT_MESSAGE_SENT
-} xsp_ws_client_loop_event_type_t;
-
-struct xsp_ws_client_loop_event_started {};
-
-struct xsp_ws_client_loop_event_stopped {};
-
-struct xsp_ws_client_loop_event_idle {};
-
-struct xsp_ws_client_loop_event_closed {
-    int status;
-};
-
-struct xsp_ws_client_loop_event_data_frame_received {
-    bool fin;
-    xsp_ws_frame_opcode_t opcode;
-    int payload_size;
-    const void* payload;
-};
-
-struct xsp_ws_client_loop_event_ping_received {
-    int payload_size;
-    const void* payload;
-};
-
-struct xsp_ws_client_loop_event_pong_received {
-    int payload_size;
-    const void* payload;
-};
-
-struct xsp_ws_client_loop_event_message_sent {
-    bool success;
-};
-
-union xsp_ws_client_loop_event_data {
-    struct xsp_ws_client_loop_event_started started;
-    struct xsp_ws_client_loop_event_stopped stopped;
-    struct xsp_ws_client_loop_event_idle idle;
-    struct xsp_ws_client_loop_event_closed closed;
-    struct xsp_ws_client_loop_event_data_frame_received data_frame_received;
-    struct xsp_ws_client_loop_event_ping_received ping_received;
-    struct xsp_ws_client_loop_event_pong_received pong_received;
-    struct xsp_ws_client_loop_event_message_sent message_sent;
-};
-
-typedef struct xsp_ws_client_loop_event {
-    xsp_ws_client_loop_event_type_t type;
-    union xsp_ws_client_loop_event_data data;
-} xsp_ws_client_loop_event_t;
-
-typedef void (*xsp_ws_client_loop_event_handler_t)(xsp_ws_client_loop_handle_t loop,
-                                                   void* ctx,
-                                                   const xsp_ws_client_loop_event_t* evt);
+typedef struct xsp_loop_event_handler {
+    on_loop_start_func_t on_loop_start;
+    on_loop_stop_func_t on_loop_stop;
+    on_loop_idle_func_t on_loop_idle;
+} xsp_loop_event_handler_t;
 
 // Default configuration.
-extern const xsp_ws_client_loop_config_t xsp_ws_client_loop_config_default;
+extern const xsp_loop_config_t xsp_loop_config_default;
 
-// Initializes the WebSocket client loop; doesn't take ownership of the client: `client` should
-// remain valid for the lifetime of the loop; it must be connected before "run". The resulting loop
-// may be used only once per connection (even if the same client is reconnected, a new loop must be
-// created).
-// TODO(vtl): Add a reinit or reset function?
-xsp_ws_client_loop_handle_t xsp_ws_client_loop_init(const xsp_ws_client_loop_config_t* config,
-                                                    xsp_ws_client_handle_t client,
-                                                    xsp_ws_client_loop_event_handler_t evt_handler,
-                                                    void* ctx);
+// Initializes loop.
+xsp_loop_handle_t xsp_loop_init(const xsp_loop_config_t* config,
+                                const xsp_loop_event_handler_t* evt_handler,
+                                void* ctx);
 
-// Cleans up (shuts down) the WebSocket client loop (does not clean up the client).
-esp_err_t xsp_ws_client_loop_cleanup(xsp_ws_client_loop_handle_t loop);
+// Cleans up (shuts down) the loop, which must not be running.
+esp_err_t xsp_loop_cleanup(xsp_loop_handle_t loop);
 
-// Runs the client loop. The loop will run until either the `xsp_ws_client_loop_stop()` is called or
-// the connection is closed.
-esp_err_t xsp_ws_client_loop_run(xsp_ws_client_loop_handle_t loop);
+// Runs the loop. The loop will run until `xsp_loop_stop()` is called.
+esp_err_t xsp_loop_run(xsp_loop_handle_t loop);
 
-// Stops the loop as soon as possible. Should only be called from "inside" the loop, i.e., inside
-// the event handler.
-esp_err_t xsp_ws_client_loop_stop(xsp_ws_client_loop_handle_t loop);
+// Stops the loop as soon as possible. Should only be called from "inside" the loop, i.e., inside an
+// event handler.
+esp_err_t xsp_loop_stop(xsp_loop_handle_t loop);
+
+//FIXME
+#if 0
 
 // Schedules the given message to be sent. On success (of scheduling),
 // `XSP_WS_CLIENT_LOOP_EVENT_MESSAGE_SENT` will be generated upon completion of the send and
--- components/xsp_loop/xsp_loop.c
+++ components/xsp_loop/xsp_loop.c
@@ -3,151 +3,169 @@
 
 #include "xsp_loop.h"
 
-//FIXME
-#if 0
+#include <stdbool.h>
 #include <stdlib.h>
-#include <sys/select.h>
-#include <sys/time.h>
 
 #include "esp_log.h"
-#include "esp_transport_utils.h"
-
-#include "xsp_ws_client_utf8.h"
 
 #include "sdkconfig.h"
 
-typedef struct xsp_ws_client_loop {
-    xsp_ws_client_loop_config_t config;
-    xsp_ws_client_handle_t client;
-    xsp_ws_client_loop_event_handler_t evt_handler;
+typedef struct xsp_loop {
+    xsp_loop_config_t config;
+    xsp_loop_event_handler_t evt_handler;
     void* ctx;
-
-    void* read_buffer;
-    int read_buffer_size;
-
-    // State that's persistent across multiple runnings of the loop.
-    bool close_sent;
-    bool close_event_sent;
-    int close_status;
-    bool sending_message;
-    bool send_is_binary;       // Only valid when sending message.
-    const void* send_message;  // Only valid when sending message. Not owned by us.
-    int send_size;             // Only valid when sending message.
-    int send_written;          // Only valid when sending message.
 
     bool is_running;
     bool should_stop;
-} xsp_ws_client_loop_t;
+} xsp_loop_t;
 
-static const char TAG[] = "WS_CLIENT_LOOP";
+static const char TAG[] = "LOOP";
 
-#if CONFIG_XSP_WS_CLIENT_LOOP_DEFAULT_MAX_FRAME_READ_SIZE < 125 ||         \
-        CONFIG_XSP_WS_CLIENT_LOOP_DEFAULT_MAX_DATA_FRAME_WRITE_SIZE < 1 || \
-        CONFIG_XSP_WS_CLIENT_LOOP_DEFAULT_POLL_TIMEOUT_MS < 0 ||           \
-        CONFIG_XSP_WS_CLIENT_LOOP_DEFAULT_READ_TIMEOUT_MS < 0 ||           \
-        CONFIG_XSP_WS_CLIENT_LOOP_DEFAULT_WRITE_TIMEOUT_MS < 0
-#error "Invalid value for CONFIG_XSP_WS_CLIENT_LOOP_DEFAULT_..."
+#if CONFIG_XSP_LOOP_DEFAULT_POLL_TIMEOUT_MS < 0
+#error "Invalid value for CONFIG_XSP_LOOP_DEFAULT_..."
 #endif
 
-const xsp_ws_client_loop_config_t xsp_ws_client_loop_config_default = {
-        CONFIG_XSP_WS_CLIENT_LOOP_DEFAULT_MAX_FRAME_READ_SIZE,
-        CONFIG_XSP_WS_CLIENT_LOOP_DEFAULT_MAX_DATA_FRAME_WRITE_SIZE,
-        CONFIG_XSP_WS_CLIENT_LOOP_DEFAULT_POLL_TIMEOUT_MS,
-        CONFIG_XSP_WS_CLIENT_LOOP_DEFAULT_READ_TIMEOUT_MS,
-        CONFIG_XSP_WS_CLIENT_LOOP_DEFAULT_WRITE_TIMEOUT_MS};
+const xsp_loop_config_t xsp_loop_config_default = {CONFIG_XSP_LOOP_DEFAULT_POLL_TIMEOUT_MS};
 
-static bool validate_config(const xsp_ws_client_loop_config_t* config) {
+static bool validate_config(const xsp_loop_config_t* config) {
     if (!config)
         return true;
-    if (config->max_frame_read_size < 125)
-        return false;
-    if (config->max_data_frame_write_size < 1)
-        return false;
     if (config->poll_timeout_ms < 0)
-        return false;
-    if (config->read_timeout_ms < 0)
-        return false;
-    if (config->write_timeout_ms < 0)
         return false;
     return true;
 }
 
-xsp_ws_client_loop_handle_t xsp_ws_client_loop_init(const xsp_ws_client_loop_config_t* config,
-                                                    xsp_ws_client_handle_t client,
-                                                    xsp_ws_client_loop_event_handler_t evt_handler,
-                                                    void* ctx) {
+xsp_loop_handle_t xsp_loop_init(const xsp_loop_config_t* config,
+                                const xsp_loop_event_handler_t* evt_handler,
+                                void* ctx) {
     if (!config)
-        config = &xsp_ws_client_loop_config_default;
+        config = &xsp_loop_config_default;
 
     if (!validate_config(config)) {
         ESP_LOGE(TAG, "Invalid config");
         return NULL;
     }
-    if (!client || !evt_handler) {
-        ESP_LOGE(TAG, "Invalid argument");
-        return NULL;
-    }
 
-    xsp_ws_client_loop_handle_t loop =
-            (xsp_ws_client_loop_handle_t)malloc(sizeof(xsp_ws_client_loop_t));
+    xsp_loop_handle_t loop = (xsp_loop_handle_t)calloc(1, sizeof(xsp_loop_t));
     if (!loop) {
         ESP_LOGE(TAG, "Allocation failed");
         return NULL;
     }
 
     loop->config = *config;
-    loop->client = client;
-    loop->evt_handler = evt_handler;
+    if (evt_handler)
+        loop->evt_handler = *evt_handler;
     loop->ctx = ctx;
-
-    loop->read_buffer = malloc(loop->config.max_frame_read_size);
-    if (!loop->read_buffer) {
-        ESP_LOGE(TAG, "Allocation failed");
-        free(loop);
-        return NULL;
-    }
-    loop->read_buffer_size = loop->config.max_frame_read_size;
-
-    loop->close_sent = false;
-    loop->close_event_sent = false;
-    loop->close_status = XSP_WS_STATUS_NONE;
-    loop->sending_message = false;
-
-    loop->is_running = false;
-    loop->should_stop = false;
 
     return loop;
 }
 
-esp_err_t xsp_ws_client_loop_cleanup(xsp_ws_client_loop_handle_t loop) {
+esp_err_t xsp_loop_cleanup(xsp_loop_handle_t loop) {
     if (!loop)
         return ESP_FAIL;
 
-    free(loop->read_buffer);
     free(loop);
     return ESP_OK;
 }
 
-// Returns true if the loop should stop.
-static bool should_stop(xsp_ws_client_loop_handle_t loop) {
-    if (loop->should_stop || loop->close_sent)
-        return true;
-
-    switch (xsp_ws_client_get_state(loop->client)) {
-    case XSP_WS_CLIENT_STATE_CLOSED:
-        // This shouldn't happen.
-        ESP_LOGE(TAG, "Loop running with client closed!");
-        return true;
-    case XSP_WS_CLIENT_STATE_OK:
+// Returns true if we should continue.
+static bool do_loop_iteration(xsp_loop_handle_t loop) {
+    if (loop->should_stop)
         return false;
-    case XSP_WS_CLIENT_STATE_FAILED:
-        return true;
-    case XSP_WS_CLIENT_STATE_FAILED_NO_CLOSE:
-        return true;
+
+    bool did_something = false;
+
+#if 0
+    // Handle pre-buffered data first, as a special case.
+    if (xsp_ws_client_has_buffered_read_data(loop->client)) {
+        do_read(loop);
+        did_something = true;
     }
-    return true;  // Shouldn't get here.
+
+    // TODO(vtl): We don't need to get the FD each iteration.
+    int fd = xsp_ws_client_get_select_fd(loop->client);
+    if (fd < 0)
+        return false;
+
+    fd_set read_fds;
+    FD_ZERO(&read_fds);
+    FD_SET(fd, &read_fds);
+    fd_set write_fds;
+    FD_ZERO(&write_fds);
+    if (loop->sending_message)
+        FD_SET(fd, &write_fds);
+
+    // TODO(vtl): We shouldn't have to do this conversion each iteration.
+    struct timeval timeout;
+    esp_transport_utils_ms_to_timeval(loop->config.poll_timeout_ms, &timeout);
+    // TODO(vtl): Possibly, we should check for error (-1) vs timeout (0).
+    if (select(fd + 1, &read_fds, &write_fds, NULL, &timeout) > 0) {
+        if (loop->sending_message && FD_ISSET(fd, &write_fds)) {
+            do_write(loop);
+            // Keep on writing while we need to and can.
+            while (loop->sending_message && select(fd + 1, NULL, &write_fds, NULL, &timeout) > 0)
+                do_write(loop);
+            did_something = true;
+            if (should_stop(loop))
+                return false;
+        }
+        if (FD_ISSET(fd, &read_fds)) {
+            do_read(loop);
+            did_something = true;
+        }
+    }
+    if (should_stop(loop))
+        return false;
+#endif
+
+    // Do idle if nothing happened.
+    if (!did_something) {
+        if (loop->evt_handler.on_loop_idle)
+            loop->evt_handler.on_loop_idle(loop, loop->ctx);
+    }
+    if (loop->should_stop)
+        return false;
+
+    return true;
 }
 
+esp_err_t xsp_loop_run(xsp_loop_handle_t loop) {
+    if (!loop)
+        return ESP_ERR_INVALID_ARG;
+    if (loop->is_running)
+        return ESP_ERR_INVALID_STATE;
+
+    loop->is_running = true;
+    loop->should_stop = false;
+    if (loop->evt_handler.on_loop_start)
+        loop->evt_handler.on_loop_start(loop, loop->ctx);
+    while (do_loop_iteration(loop))
+        ;  // Nothing.
+    if (loop->evt_handler.on_loop_stop)
+        loop->evt_handler.on_loop_stop(loop, loop->ctx);
+    loop->is_running = false;
+
+    return ESP_OK;
+}
+
+esp_err_t xsp_loop_stop(xsp_loop_handle_t loop) {
+    if (!loop)
+        return ESP_ERR_INVALID_ARG;
+    if (!loop->is_running)
+        return ESP_ERR_INVALID_STATE;
+
+    loop->should_stop = true;
+    return ESP_OK;
+}
+
+//FIXME
+#if 0
+#include <sys/select.h>
+#include <sys/time.h>
+
+#include "esp_transport_utils.h"
+
+// Returns true if the loop should stop.
 static void do_read(xsp_ws_client_loop_handle_t loop) {
     bool fin;
     xsp_ws_frame_opcode_t opcode;
@@ -283,179 +301,5 @@ static void do_idle(xsp_ws_client_loop_handle_t loop) {
 
 static bool is_closed(xsp_ws_client_loop_handle_t loop) {
     return loop->close_sent || xsp_ws_client_get_state(loop->client) != XSP_WS_CLIENT_STATE_OK;
-}
-
-// Returns true if we should continue.
-static bool do_loop_iteration(xsp_ws_client_loop_handle_t loop) {
-    if (should_stop(loop))
-        return false;
-
-    bool did_something = false;
-
-    // Handle pre-buffered data first, as a special case.
-    if (xsp_ws_client_has_buffered_read_data(loop->client)) {
-        do_read(loop);
-        did_something = true;
-    }
-
-    // TODO(vtl): We don't need to get the FD each iteration.
-    int fd = xsp_ws_client_get_select_fd(loop->client);
-    if (fd < 0)
-        return false;
-
-    fd_set read_fds;
-    FD_ZERO(&read_fds);
-    FD_SET(fd, &read_fds);
-    fd_set write_fds;
-    FD_ZERO(&write_fds);
-    if (loop->sending_message)
-        FD_SET(fd, &write_fds);
-
-    // TODO(vtl): We shouldn't have to do this conversion each iteration.
-    struct timeval timeout;
-    esp_transport_utils_ms_to_timeval(loop->config.poll_timeout_ms, &timeout);
-    // TODO(vtl): Possibly, we should check for error (-1) vs timeout (0).
-    if (select(fd + 1, &read_fds, &write_fds, NULL, &timeout) > 0) {
-        if (loop->sending_message && FD_ISSET(fd, &write_fds)) {
-            do_write(loop);
-            // Keep on writing while we need to and can.
-            while (loop->sending_message && select(fd + 1, NULL, &write_fds, NULL, &timeout) > 0)
-                do_write(loop);
-            did_something = true;
-            if (should_stop(loop))
-                return false;
-        }
-        if (FD_ISSET(fd, &read_fds)) {
-            do_read(loop);
-            did_something = true;
-        }
-    }
-    if (should_stop(loop))
-        return false;
-
-    // Do idle if we didn't read or write.
-    if (!did_something)
-        do_idle(loop);
-
-    return true;
-}
-
-esp_err_t xsp_ws_client_loop_run(xsp_ws_client_loop_handle_t loop) {
-    if (!loop)
-        return ESP_ERR_INVALID_ARG;
-    if (loop->is_running)
-        return ESP_ERR_INVALID_STATE;
-
-    loop->is_running = true;
-    loop->should_stop = false;
-    {
-        xsp_ws_client_loop_event_t evt;
-        evt.type = XSP_WS_CLIENT_LOOP_EVENT_STARTED;
-        loop->evt_handler(loop, loop->ctx, &evt);
-    }
-    while (do_loop_iteration(loop))
-        ;  // Nothing.
-
-    // NOTE: The repeated (explicit or implicit) calls to xsp_ws_client_get_state() below are
-    // intentional: it guards against the possibility that the state changes in between the
-    // different locations.
-
-    if (loop->sending_message && is_closed(loop))
-        send_message_failed(loop);
-
-    if (!loop->close_sent && xsp_ws_client_get_state(loop->client) == XSP_WS_CLIENT_STATE_FAILED) {
-        // TODO(vtl): Currently, the only reason for XSP_WS_CLIENT_STATE_FAILED are protocol errors,
-        // but this may change.
-        xsp_ws_client_write_close_frame(loop->client, XSP_WS_STATUS_CLOSE_PROTOCOL_ERROR, NULL,
-                                        loop->config.write_timeout_ms);
-        loop->close_sent = true;
-        loop->close_status = XSP_WS_STATUS_CLOSE_PROTOCOL_ERROR;
-    }
-
-    if (!loop->close_event_sent && is_closed(loop)) {
-        loop->close_event_sent = true;
-
-        xsp_ws_client_loop_event_t evt;
-        evt.type = XSP_WS_CLIENT_LOOP_EVENT_CLOSED;
-        evt.data.closed.status = loop->close_status ? loop->close_status
-                                                    : XSP_WS_STATUS_CLOSE_RESERVED_ABNORMAL_CLOSURE;
-        loop->evt_handler(loop, loop->ctx, &evt);
-    }
-
-    {
-        xsp_ws_client_loop_event_t evt;
-        evt.type = XSP_WS_CLIENT_LOOP_EVENT_STOPPED;
-        loop->evt_handler(loop, loop->ctx, &evt);
-    }
-
-    loop->is_running = false;
-
-    return ESP_OK;
-}
-
-esp_err_t xsp_ws_client_loop_stop(xsp_ws_client_loop_handle_t loop) {
-    if (!loop)
-        return ESP_ERR_INVALID_ARG;
-    if (!loop->is_running)
-        return ESP_ERR_INVALID_STATE;
-
-    loop->should_stop = true;
-    return ESP_OK;
-}
-
-esp_err_t xsp_ws_client_loop_send_message(xsp_ws_client_loop_handle_t loop,
-                                          bool binary,
-                                          int message_size,
-                                          const void* message) {
-    if (!loop || message_size < 0 || (message_size > 0 && !message))
-        return ESP_ERR_INVALID_ARG;
-    if (!loop->is_running || loop->sending_message)
-        return ESP_ERR_INVALID_STATE;
-    if (xsp_ws_client_get_state(loop->client) != XSP_WS_CLIENT_STATE_OK)
-        return ESP_FAIL;
-
-    loop->sending_message = true;
-    loop->send_is_binary = binary;
-    loop->send_message = message;
-    loop->send_size = message_size;
-    loop->send_written = 0;
-    return ESP_OK;
-}
-
-esp_err_t xsp_ws_client_loop_close(xsp_ws_client_loop_handle_t loop, int close_status) {
-    if (!loop || !xsp_ws_is_valid_close_frame_status(close_status))
-        return ESP_ERR_INVALID_ARG;
-    if (!loop->is_running)
-        return ESP_ERR_INVALID_STATE;
-
-    // Don't report an error if we can't actually send a close frame. Note that in the
-    // XSP_WS_CLIENT_STATE_FAILED case, we'll send a close frame automatically.
-    if (xsp_ws_client_get_state(loop->client) != XSP_WS_CLIENT_STATE_OK) {
-        // Note: The loop will stop automatically in this case.
-        return ESP_OK;
-    }
-
-    // Note: We'll avoid reporting an error if we're already sent a close frame, since it may have
-    // been due to echoing a close frame from the server. (Moreover, an idempotent close is nice to
-    // have.)
-    if (loop->close_sent)
-        return ESP_OK;
-
-    xsp_ws_client_write_close_frame(loop->client, close_status, NULL,
-                                    loop->config.write_timeout_ms);
-    loop->close_sent = true;
-    return ESP_OK;
-}
-
-esp_err_t xsp_ws_client_loop_ping(xsp_ws_client_loop_handle_t loop,
-                                  int payload_size,
-                                  const void* payload) {
-    if (!loop || payload_size < 0 || payload_size > 125 || (payload_size != 0 && !payload))
-        return ESP_ERR_INVALID_ARG;
-    if (!loop->is_running)
-        return ESP_ERR_INVALID_STATE;
-
-    return xsp_ws_client_write_frame(loop->client, true, XSP_WS_FRAME_OPCODE_PING, payload_size,
-                                     payload, loop->config.write_timeout_ms);
 }
 #endif
