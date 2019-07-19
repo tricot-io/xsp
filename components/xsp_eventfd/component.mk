390f41bda2162f80c9e8b19a822cb522efad4cc1
Viet-Trung Luu <vtl@tricot.io>
2019-07-18 17:47:36 -0700

    [eventfd] Add make support.

---
+++ components/xsp_eventfd/component.mk
@@ -0,0 +1,5 @@
+# Copyright 2019 Tricot Inc.
+# Use of this source code is governed by the license in the LICENSE file.
+
+# Uses default behavior: names component for the directory, builds all source files, and adds
+# include subdirectory to include path.
